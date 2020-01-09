55ee6d129dd60290b0bba614ac63b5201ef68045
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-09 00:53:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,9 @@
 def find_item_by_name_in_collection(name, collection)
   index = 0
   while index < collection.length do
-    collection[index][:item] == name ? collection[index] : index += 1
+    if collection[index][:item] == name
+      return collection[index]
+    else index += 1
   end
   # Implement me first!
   # Consult README for inputs and outputs
