41c356f5c35aa885a8c2c4afa13dc5591439487a
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-09 00:54:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,6 +4,7 @@ def find_item_by_name_in_collection(name, collection)
     if collection[index][:item] == name
       return collection[index]
     else index += 1
+    end
   end
   # Implement me first!
   # Consult README for inputs and outputs
